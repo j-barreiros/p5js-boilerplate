14a80f19f7c402e5a133770504e6099c50de9b6e
J-Novaes <joaovictor.nb21@hotmail.com>
2020-05-04 02:56:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# What is this ?
+A simple boilerplate for p5js
+
+# Installation
+
+`npm i p5js-boilerplate --save`
---
+++ js/main.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    //noLoop();
+    //frameRate(60);
+}
+
+function draw() {
+    //Write your code here
+}
